73e90f33ea544d79596ed07d49a6332ce29923b6
Jamie Houston <jamie.houston@salesforce.com>
2024-03-16 22:03:24 -0500

    cleanup strategy

--- crapssim/strategy/strategy.py
+++ crapssim/strategy/strategy.py
@@ -13,14 +13,22 @@ Fundamental Strategies
 """
 
 
-class RollStatus(Enum):
-    NEW_SHOOTER = "on_new_shooter",
-    COMING_OUT = "on_coming_out",
-    POINT_SET = "on_point_set",
-    POINT_HIT = "on_point_hit",
-    SEVEN_OUT = "on_seven_out",
-    ACTIVE_POINT = "on_active_point",
-    ANY_STATUS = "on_any_status",
+class RollEvent(Enum):
+    BEFORE_ROLL_CALLBACK = "before_roll_callback"
+    NEW_SHOOTER = "on_new_shooter"
+    COMING_OUT = "on_coming_out"
+    POINT_SET = "on_point_set"
+    POINT_HIT = "on_point_hit"
+    SEVEN_OUT = "on_seven_out"
+    ACTIVE_POINT = "on_active_point"
+    ANY_STATUS = "on_any_status"
+
+class BetEvent(Enum):
+    BEFORE_BET_RESULT = "before_bet_result"
+    ON_WIN = "on_win"
+    ON_LOSS = "on_loss"
+    ON_PUSH = "on_push"
+    AFTER_BET_RESULT = "after_bet_result"
 
 
 class Strategy:
@@ -30,6 +38,7 @@ class Strategy:
     Do NOT overwrite update bets unless you don't want the other methods to work
 
     Status of roll in chronological order:
+    0. before_roll_callback - before any callbacks
     1. on_new_shooter - first roll of shooter
     2. on_coming_out - point not set
     3. on_point_set - first roll after point is set
@@ -39,7 +48,7 @@ class Strategy:
     7. on_any_status - Any time in roll
 
     Bet results - Called for each existing bet on the table
-    1. before_bet_result
+    0. before_bet_result
     1. on_win - win
     2. on_loss - loss
     3. on_push - no change
@@ -60,80 +69,36 @@ class Strategy:
 
         self.__handle_roll_callbacks(player, table)
 
+    def handle_roll_event(self, roll_event, *args):
+        method = getattr(self, roll_event.value, None)
+        if method:
+            method(*args)  # Call the method directly if it exists
+
     def __handle_roll_callbacks(self, player, table):
-        self.before_roll_callback(player, table, table.last_roll)
+        self.handle_roll_event(RollEvent.BEFORE_ROLL_CALLBACK, player, table)
         if table.point.is_on():
             if table.last_roll == table.point.number:
-                self.on_point_set(player, table, table.last_roll)
-            self.on_active_point(player, table)
+                self.handle_roll_event(RollEvent.POINT_SET, player, table, table.last_roll)
+            self.handle_roll_event(RollEvent.ACTIVE_POINT, player, table)
         else:
             if table.last_roll is None:
-                self.on_new_shooter(player, table)
+                self.handle_roll_event(RollEvent.NEW_SHOOTER, player, table)
             elif table.last_roll == 7:
-                self.on_seven_out(player, table)
+                self.handle_roll_event(RollEvent.SEVEN_OUT, player, table)
             elif table.last_roll not in [2, 3, 11, 12]:
-                self.on_point_hit(player, table, table.last_roll)
-            self.on_coming_out(player, table)
-        self.on_any_status(player, table)
+                self.handle_roll_event(RollEvent.POINT_HIT, player, table, table.last_roll)
+            self.handle_roll_event(RollEvent.COMING_OUT, player, table)
+        self.handle_roll_event(RollEvent.ANY_STATUS, player, table)
 
     def __handle_bet_callbacks(self, player, table, last_bets):
         if last_bets is not None:
             for bet, bet_info in last_bets.items():
-                self.before_bet_result(player, table, bet_info)
+                self.handle_roll_event(BetEvent.BEFORE_BET_RESULT, player, table, bet_info)
                 match bet_info.status:
                     case BetStatus.PUSH:
-                        self.on_push(player, table, bet_info)
+                        self.handle_roll_event(BetEvent.ON_PUSH, player, table, bet_info)
                     case BetStatus.WIN:
-                        self.on_win(player, table, bet_info)
+                        self.handle_roll_event(BetEvent.ON_WIN, player, table, bet_info)
                     case BetStatus.LOSE:
-                        self.on_loss(player, table, bet_info)
-                self.after_bet_result(player, table, bet_info)
-
-    def before_bet_result(self, player, table, bet_info):
-        # Called before any bet result
-        pass
-
-    def after_bet_result(self, player, table, bet_info):
-        # Called with any bet result
-        pass
-
-    def on_win(self, player, table, winning_bet):
-        # Called with any winning bet
-        pass
-
-    def on_loss(self, player, table, losing_bet_info):
-        # Called with any losing bet
-        pass
-
-    def on_push(self, player, table, bet_info):
-        # Called with any push bet
-        pass
-
-    def on_seven_out(self, player, table):
-        # called after 7 out
-        pass
-
-    def before_roll_callback(self, player, table, last_roll):
-        # Called before any roll
-        pass
-
-    def on_point_set(self, player, table, last_roll):
-        # When the point starts
-        pass
-
-    def on_point_hit(self, player, table, last_roll):
-        pass
-
-    def on_new_shooter(self, player, table):
-        # New shooter coming out
-        pass
-
-    def on_any_status(self, player, table):
-        pass
-
-    def on_active_point(self, player, table):
-        pass
-
-    def on_coming_out(self, player, table):
-        # When the point is off
-        pass
+                        self.handle_roll_event(BetEvent.ON_LOSS, player, table, bet_info)
+                self.handle_roll_event(BetEvent.AFTER_BET_RESULT, player, table, bet_info)
